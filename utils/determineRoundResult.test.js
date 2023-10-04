eb7b62e87c1a8f81edebabce083173daae7dac2e
trendSetters01 <142847732+trendSetters01@users.noreply.github.com>
2023-10-04 04:44:53 +0000

    test(utils): Comprehensive tests for determineRoundResult function
    - Covered all possible outcomes (draw, user win, bot win) for rock-paper-scissors logic.
    - Utilized constants for choices and results to ensure consistency in tests.

--- utils/determineRoundResult.test.js
+++ utils/determineRoundResult.test.js
@@ -1,17 +1,53 @@
-import { determineRoundResult } from './index.js';
+import { determineRoundResult, CHOICES, RESULTS } from './index.js';
 
 describe('determineRoundResult', () => {
 
-  it('should return "draw" when both user and bot choose ROCK', () => {
-    const result = determineRoundResult('rock', 'rock');
-    expect(result).toBe('draw');
+  // Draw scenarios
+  it('should return DRAW when both user and bot choose ROCK', () => {
+    const result = determineRoundResult(CHOICES.ROCK, CHOICES.ROCK);
+    expect(result).toBe(RESULTS.DRAW);
   });
 
-  it('should return "user" when user chooses ROCK and bot chooses SCISSORS', () => {
-    const result = determineRoundResult('rock', 'scissors');
-    expect(result).toBe('user');
+  it('should return DRAW when both user and bot choose PAPER', () => {
+    const result = determineRoundResult(CHOICES.PAPER, CHOICES.PAPER);
+    expect(result).toBe(RESULTS.DRAW);
   });
 
-  // ... more tests for other combinations
+  it('should return DRAW when both user and bot choose SCISSORS', () => {
+    const result = determineRoundResult(CHOICES.SCISSORS, CHOICES.SCISSORS);
+    expect(result).toBe(RESULTS.DRAW);
+  });
+
+  // User win scenarios
+  it('should return USER when user chooses ROCK and bot chooses SCISSORS', () => {
+    const result = determineRoundResult(CHOICES.ROCK, CHOICES.SCISSORS);
+    expect(result).toBe(RESULTS.USER);
+  });
+
+  it('should return USER when user chooses PAPER and bot chooses ROCK', () => {
+    const result = determineRoundResult(CHOICES.PAPER, CHOICES.ROCK);
+    expect(result).toBe(RESULTS.USER);
+  });
+
+  it('should return USER when user chooses SCISSORS and bot chooses PAPER', () => {
+    const result = determineRoundResult(CHOICES.SCISSORS, CHOICES.PAPER);
+    expect(result).toBe(RESULTS.USER);
+  });
+
+  // Bot win scenarios
+  it('should return BOT when user chooses ROCK and bot chooses PAPER', () => {
+    const result = determineRoundResult(CHOICES.ROCK, CHOICES.PAPER);
+    expect(result).toBe(RESULTS.BOT);
+  });
+
+  it('should return BOT when user chooses PAPER and bot chooses SCISSORS', () => {
+    const result = determineRoundResult(CHOICES.PAPER, CHOICES.SCISSORS);
+    expect(result).toBe(RESULTS.BOT);
+  });
+
+  it('should return BOT when user chooses SCISSORS and bot chooses ROCK', () => {
+    const result = determineRoundResult(CHOICES.SCISSORS, CHOICES.ROCK);
+    expect(result).toBe(RESULTS.BOT);
+  });
 
 });
